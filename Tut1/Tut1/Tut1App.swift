6a69a13580ed871803a9c05fb175e3c9cdd4f0ba
ClemonBalasuriya <avishkaclemon@gmail.com>
2025-10-17 13:04:51 +0530

    first commit

---
+++ Tut1/Tut1/Tut1App.swift
@@ -0,0 +1,17 @@
+//
+//  Tut1App.swift
+//  Tut1
+//
+//  Created by Clemon Balasuriya on 2025-10-17.
+//
+
+import SwiftUI
+
+@main
+struct Tut1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
